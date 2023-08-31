0f972f37d6a788ffd85645f00f8d78a8b4a682e5
sergio-usma <sergioandresusma@hotmail.com>
2023-08-30 21:55:43 -0500

    Add Teacher class

---
+++ teacher.rb
@@ -0,0 +1,13 @@
+require './person'
+
+# Creating class Teacher
+class Teacher < Person
+  def initialize(age, specialization, name: 'Unknown', parent_permission: true)
+    super(name, age, parent_permission)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end
