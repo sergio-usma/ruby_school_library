427a439d2b170c6c3363c36599681eaaec96f272
Sergio Usma <sergioandresusma@hotmail.com>
2023-09-01 21:35:09 -0500

    FIX rubocop offenses

---
+++ classes/person.rb
@@ -0,0 +1,39 @@
+require_relative 'nameable'
+require_relative 'capitalize_decorator'
+require_relative 'trimmer_decorator'
+
+class Person < Nameable
+  attr_reader :id
+  attr_accessor :name, :age, :rentals
+
+  def initialize(age, name: 'Unknown', parent_permission: true)
+    super()
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age.to_i
+    @parent_permission = parent_permission
+    @rentals = []
+  end
+
+  def correct_name
+    @name
+  end
+
+  private
+
+  def of_age?
+    @age >= 18
+  end
+
+  public
+
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+
+  def add_rental(date, book)
+    Rental.new(date, book, self)
+    @rentals << rental
+    rental
+  end
+end
