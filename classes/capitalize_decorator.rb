50dbc0d0430573f8a7d7148132c46ddc616f7871
Sergio Usma <sergioandresusma@hotmail.com>
2023-09-09 12:24:11 -0500

    Add require_relative and refactor according CR

--- classes/capitalize_decorator.rb
+++ classes/capitalize_decorator.rb
@@ -2,7 +2,6 @@ require_relative 'base_decorator'
 
 class CapitalizeDecorator < BaseDecorator
   def correct_name
-    original_name = @nameable.correct_name
-    original_name.capitalize
+    @nameable.correct_name.capitalize.to_s
   end
 end
