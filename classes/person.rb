b66557073ea4775ce6ebc4623cd79c8c5952fb96
Sergio Usma <sergioandresusma@hotmail.com>
2023-09-09 15:48:17 -0500

    Add require_relative and refactor according CR

--- classes/person.rb
+++ classes/person.rb
@@ -1,12 +1,13 @@
+require_relative 'book'
 require_relative 'nameable'
-require_relative 'capitalize_decorator'
-require_relative 'trimmer_decorator'
+require_relative 'rental'
+require_relative 'student'
 
 class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age, :rentals
 
-  def initialize(age, name: 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     super()
     @id = Random.rand(1..1000)
     @name = name
@@ -15,25 +16,23 @@ class Person < Nameable
     @rentals = []
   end
 
+  def can_use_services?(parent_permission: true)
+    return false if of_age? && parent_permission
+
+    true
+  end
+
   def correct_name
     @name
   end
 
-  private
-
-  def of_age?
-    @age >= 18
-  end
-
-  public
-
-  def can_use_services?
-    of_age? || @parent_permission
-  end
-
   def add_rental(date, book)
     Rental.new(date, book, self)
-    @rentals << rental
-    rental
   end
+end
+
+  private
+
+def of_age?
+  @age >= 18
 end
