133fed2b7464f0fc48c6508115855ec9b273391e
Sergio Usma <sergioandresusma@hotmail.com>
2023-09-09 12:17:29 -0500

    Add require_relative according CR

--- classes/student.rb
+++ classes/student.rb
@@ -1,21 +1,22 @@
+require_relative 'book'
+require_relative 'classroom'
 require_relative 'person'
+require_relative 'teacher'
 
-# Creating class Student
 class Student < Person
-  attr_accessor :classroom, :parent_permission
+  attr_accessor :classroom
 
-  def initialize(age, classroom, name: 'Unknown', parent_permission: true)
-    super(age, name: name, parent_permission: parent_permission)
+  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
     @classroom = classroom
-    classroom.add_student(self)
+  end
+
+  def play_hooky
+    '¯(ツ)/¯'
   end
 
   def class_room=(classroom)
     @classroom = classroom
     classroom.students.push(self) unless classroom.students.include?(self)
-  end
-
-  def play_hooky
-    '¯(ツ)/¯'
   end
 end
