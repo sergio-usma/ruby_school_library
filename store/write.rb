3c9983e9450edf756e17b6288f0bbc576a448838
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-09-13 11:11:52 -0600

    Implement Method Store that receives data from the main app and pass it to write_books, write_person and write_rentals as arguments

--- store/write.rb
+++ store/write.rb
@@ -9,12 +9,12 @@ class Write
     FileUtils.touch('./data/rentals.json') unless File.exist?('./data/rentals.json') && file_name == 'rentals'
   end
 
-  #   def to_store
-  #     write_books
-  #     write_person
-  #     write_rentals
-  #     puts 'All Records were saved !'
-  #   end
+     def store(people,books,rentals)
+       write_books(books)
+       write_person(people)
+       write_rentals(rentals)
+       puts 'All Records were saved !'
+     end
 
   def write_books(books)
     puts 'Saving books into storage ...'
