685aab13e31306720d013ad472531df2d6954e82
Sergio Usma <sergioandresusma@hotmail.com>
2023-09-13 10:36:28 -0500

    Modify params order to avoid bugs in Teacher class

--- classes/teacher.rb
+++ classes/teacher.rb
@@ -3,8 +3,8 @@ require_relative 'person'
 class Teacher < Person
   attr_accessor :specialization
 
-  def initialize(age, specialization, name = 'Unknown', parent_permission: true, type: 'Teacher')
-    super(age, name, parent_permission: parent_permission)
+  def initialize(name, age, specialization, parent_permission: true, type: 'Teacher')
+    super(name, age, parent_permission: parent_permission)
     @specialization = specialization
     @type = type
   end
