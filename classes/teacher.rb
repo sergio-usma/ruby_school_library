ce0fe8b4adaba85b60c7117aa03b099602a51882
Sergio Usma <sergioandresusma@hotmail.com>
2023-09-13 09:53:50 -0500

    Add type param for Teacher

--- classes/teacher.rb
+++ classes/teacher.rb
@@ -3,9 +3,10 @@ require_relative 'person'
 class Teacher < Person
   attr_accessor :specialization
 
-  def initialize(age, specialization, name = 'Unknown', parent_permission: true)
+  def initialize(age, specialization, name = 'Unknown', parent_permission: true, type: 'Teacher')
     super(age, name, parent_permission: parent_permission)
     @specialization = specialization
+    @type = type
   end
 
   def can_use_services?
