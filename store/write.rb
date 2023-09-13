62a691f6f8d20f6f54ee54c31f2581a1ce694c70
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-09-12 19:04:38 -0600

    Implement write_person method for saving data in the store

--- store/write.rb
+++ store/write.rb
@@ -9,6 +9,7 @@ class Write
 
   def to_store
     write_books
+    write_person
     puts "All Records were saved !"
   end
 
@@ -25,6 +26,28 @@ class Write
 
     json_data = JSON.generate(book_data)
     store = File.open('./data/books.json', "w") { | file |
+      file.write json_data
+    }
+  end
+
+  def write_person
+    people_data = []
+    @people.each { | person |
+      person_hash = {
+        'id' => person.id,
+        'type' => person.class,
+        'name' => person.name,
+        'age' => person.age,
+      }
+      if person.class == Student
+          person_hash['classroom'] = person.classroom
+      else
+        person_hash['specialization'] = person.specialization
+      end
+      people_data.push(person_hash)
+    }
+    json_data = JSON.generate(people_data)
+    store = File.open('./data/person.json', 'w') { | file |
       file.write json_data
     }
   end
