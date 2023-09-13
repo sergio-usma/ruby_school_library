d54cd26723de13fc320a98b30358b2d8222d6bed
Sergio Usma <sergioandresusma@hotmail.com>
2023-09-13 09:54:07 -0500

    Add type param for Student

--- classes/student.rb
+++ classes/student.rb
@@ -6,9 +6,10 @@ require_relative 'teacher'
 class Student < Person
   attr_accessor :classroom
 
-  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
+  def initialize(age, classroom, name = 'Unknown', parent_permission: true, type: 'Student')
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
+    @type = type
   end
 
   def play_hooky
