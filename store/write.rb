bcbd9132933454e03201c457929132d72992581c
Sergio Usma <sergioandresusma@hotmail.com>
2023-09-13 12:00:08 -0500

    Refactor Write.rb using fileutils ruby object

--- store/write.rb
+++ store/write.rb
@@ -1,38 +1,40 @@
 require 'json'
+require 'fileutils'
 
 class Write
-  def initialize(people, books, rentals)
-    @people = people
-    @books = books
-    @rentals = rentals
+  def check_file(file_name)
+    FileUtils.mkdir_p('./data')
+    FileUtils.touch('./data/books.json') unless File.exist?('./data/books.json') && file_name == 'books'
+    FileUtils.touch('./data/person.json') unless File.exist?('./data/person.json') && file_name == 'person'
+    FileUtils.touch('./data/rentals.json') unless File.exist?('./data/rentals.json') && file_name == 'rentals'
   end
 
-  def to_store
-    write_books
-    write_person
-    write_rentals
-    puts 'All Records were saved !'
-  end
+  #   def to_store
+  #     write_books
+  #     write_person
+  #     write_rentals
+  #     puts 'All Records were saved !'
+  #   end
 
-  def write_books
+  def write_books(books)
     puts 'Saving books into storage ...'
     book_data = []
-
-    @books.each do |book|
+    books.each do |book|
       book_data.push({
                        'title' => book.title,
                        'author' => book.author
                      })
     end
+    return if book_data.empty?
 
-    json_data = JSON.generate(book_data)
-    File.write('./data/books.json', json_data)
+    check_file('books')
+    File.write('./data/books.json', JSON.pretty_generate(book_data))
   end
 
-  def write_person
+  def write_person(people)
     puts 'Saving people into storage ...'
     people_data = []
-    @people.each do |person|
+    people.each do |person|
       person_hash = {
         'id' => person.id,
         'type' => person.class,
@@ -46,14 +48,16 @@ class Write
       end
       people_data.push(person_hash)
     end
-    json_data = JSON.generate(people_data)
-    File.write('./data/person.json', json_data)
+    return if people_data.empty?
+
+    check_file('person')
+    File.write('./data/person.json', JSON.pretty_generate(people_data))
   end
 
-  def write_rentals
+  def write_rentals(rentals)
     puts 'Saving rentals into storage ...'
     rentals_data = []
-    @rentals.each do |rental|
+    rentals.each do |rental|
       rental_hash = {
         'date' => rental.date,
         'title' => rental.book.title,
@@ -70,7 +74,9 @@ class Write
       end
       rentals_data.push(rental_hash)
     end
-    json_data = JSON.generate(rentals_data)
-    File.write('./data/rentals.json', json_data)
+    return if rentals_data.empty?
+
+    check_file('rentals')
+    File.write('./data/rentals.json', JSON.pretty_generate(rentals_data))
   end
 end
