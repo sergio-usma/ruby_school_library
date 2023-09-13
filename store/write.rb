c350550b66956de727e9795ed3cb7faedb8195df
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-09-12 19:54:07 -0600

    Implement Method Write_rentals: save date,title,author,id,name,age,and type into rentals.json file

--- store/write.rb
+++ store/write.rb
@@ -10,6 +10,7 @@ class Write
   def to_store
     write_books
     write_person
+    write_rentals
     puts "All Records were saved !"
   end
 
@@ -31,6 +32,7 @@ class Write
   end
 
   def write_person
+    puts "Saving people into storage ..."
     people_data = []
     @people.each { | person |
       person_hash = {
@@ -51,4 +53,31 @@ class Write
       file.write json_data
     }
   end
+
+  def write_rentals
+    puts "Saving rentals into storage ..."
+    rentals_data = []
+    @rentals.each { | rental | 
+      rental_hash = {
+        'date' => rental.date,
+        'title' => rental.book.title,
+        'author' => rental.book.author,
+        'id' => rental.person.id,
+        'name' => rental.person.name,
+        'age' => rental.person.age,
+        'type' => rental.person.class,
+      }
+      if rental.person.class == Student
+        rental_hash['classroom'] = rental.person.classroom
+      else
+        rental_hash['specialization'] = rental.person.specialization
+      end
+      rentals_data.push(rental_hash)
+    }
+    json_data = JSON.generate(rentals_data)
+    store = File.open('./data/rentals.json', 'w') { | file |
+      file.write json_data
+    }
+  end
+
 end
